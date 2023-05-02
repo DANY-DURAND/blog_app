bb4e04014db9b2c477d26bf232dd6c02881ed342
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2023-05-02 17:23:42 +0800

    Add a index function for view

--- WeBlog/blog/views.py
+++ WeBlog/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+error_message = "Invalid Credentials"
+def index(request):
+    return render( request, "login.html",
+                    {
+                        "error_message": error_message,
+                    })
