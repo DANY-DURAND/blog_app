d7209f87831eb63c80ccf4bcd42c35096505d3e9
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2023-05-02 17:25:31 +0800

    Update views.py

--- WeBlog/blog/views.py
+++ WeBlog/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 error_message = "Invalid Credentials"
 def index(request):
-    return render( request, "index.html",
+    return render( request, "blog/index.html",
                     {
                         "error_message": error_message,
                     })
